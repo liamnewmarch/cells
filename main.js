38602d9bf68e5bde7b25be1287943e65875d4732
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2020-03-30 21:09:45 +0100

    Modernise the code

--- main.js
+++ main.js
@@ -1,115 +1,94 @@
-(function(global) {
-    'use strict';
+class CellularAutomata extends EventTarget {
+  constructor({ canvas, scale = 4 } = {}) {
+    super();
+    Object.assign(this, { canvas, scale });
+    this.context = canvas.getContext('2d');
+    this.paused = true;
+    this.start();
+    addEventListener('resize', () => this.reset());
+  }
 
-    var cells, context, frameRequest, height, width;
+  cellAlive(x, y) {
+    x = (x + this.width) % this.width;
+    y = (y + this.height) % this.height;
+    return this.cells[y][x];
+  }
 
-    function CellularAutomata(canvas) {
+  cellAliveNeighbourCount(x, y) {
+    return [
+      this.cellAlive(x - 1, y - 1),
+      this.cellAlive(x + 0, y - 1),
+      this.cellAlive(x + 1, y - 1),
+      this.cellAlive(x - 1, y + 0),
+      this.cellAlive(x + 1, y + 0),
+      this.cellAlive(x - 1, y + 1),
+      this.cellAlive(x + 0, y + 1),
+      this.cellAlive(x + 1, y + 1),
+    ].reduce((count, alive) => count + Number(alive), 0);
+  }
 
-        var x, y;
+  iterate() {
+    requestAnimationFrame(() => this.iterate());
+    if (this.paused) return;
 
-        if (this === global) {
-            return new CellularAutomata(canvas);
+    const nextCells = [];
+    for (let y = 0; y < this.height; y++) {
+      nextCells[y] = [];
+
+      for (let x = 0; x < this.width; x++) {
+        const neighbours = this.cellAliveNeighbourCount(x, y);
+
+        if (this.cells[y][x]) {
+          nextCells[y][x] = neighbours >= 2 && neighbours <= 3;
+        } else {
+          nextCells[y][x] = neighbours === 3;
         }
-
-        cells = [];
-
-        width = Math.ceil(canvas.width);
-        height = Math.ceil(canvas.height);
-
-        context = canvas.getContext('2d');
-
-        for (y = 0; y < height; y++) {
-            cells.push([]);
-            for (x = 0; x < width; x++) {
-                cells[y].push(0);
-            }
-        }
-
-        this.randomise();
-        this.iterate();
+      }
     }
 
-    CellularAutomata.prototype.paintToCanvas = function() {
+    this.cells = nextCells;
+    this.render();
+  }
 
-        var x, y, image, imageData, i, width, height;
+  render() {
+    const image = this.context.createImageData(this.width, this.height);
+    const { data } = image;
 
-        width = context.canvas.width;
-        height = context.canvas.height;
-        image = context.createImageData(width, height);
-		imageData = image.data;
-
-        for (y = 0; y < height; y++) {
-            for (x = 0; x < width; x++) {
-                i = 4 * (y * width + x);
-                if (cells[y][x]) {
-                    imageData[i + 0] = 255;
-                    imageData[i + 1] = 255;
-                    imageData[i + 2] = 255;
-                    imageData[i + 3] = 255;
-                }
-            }
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        const i = 4 * (y * this.width + x);
+        if (this.cells[y][x]) {
+          data[i + 0] = data[i + 1] = data[i + 2] = data[i + 3] = 255;
         }
+      }
+    }
+    this.context.putImageData(image, 0, 0);
+  }
 
-        context.putImageData(image, 0, 0);
-    };
+  reset() {
+    this.cells = [];
+    this.width = this.context.canvas.width = Math.ceil(window.innerWidth / this.scale);
+    this.height = this.context.canvas.height = Math.ceil(window.innerHeight / this.scale);
 
-    CellularAutomata.prototype.checkCell = function(x, y) {
-        if (x < 0 || y < 0 || y > cells.length - 1 || x > cells[y].length - 1) {
-            return 0;
-        }
-        return cells[y][x] ? 1 : 0;
-    };
+    for (var y = 0; y < this.height; y++) {
+      this.cells[y] = [];
+      for (var x = 0; x < this.width; x++) {
+        this.cells[y][x] = Math.floor(16 * Math.random()) === 0;
+      }
+    }
 
-    CellularAutomata.prototype.iterate = function() {
-        var sum, nextCells = [], self = this;
+    this.render();
+  }
 
-        requestAnimationFrame(function() {
-            self.iterate();
-        });
+  start() {
+    this.paused = false;
+    this.reset();
+    requestAnimationFrame(() => this.iterate());
+  }
+}
 
-        for (var y = 0; y < height; y++) {
-
-            nextCells.push([]);
-
-            for (var x = 0; x < width; x++) {
-
-                sum = 0;
-                sum += this.checkCell(x - 1, y - 1);
-                sum += this.checkCell(x, y - 1);
-                sum += this.checkCell(x + 1, y - 1);
-                sum += this.checkCell(x - 1, y);
-                sum += this.checkCell(x + 1, y);
-                sum += this.checkCell(x - 1, y + 1);
-                sum += this.checkCell(x, y + 1);
-                sum += this.checkCell(x + 1, y + 1);
-
-                if (cells[y][x]) {
-                    nextCells[y][x] = sum >= 2 && sum <= 3;
-                } else {
-                    nextCells[y][x] = sum === 3;
-                }
-            }
-        }
-
-        cells = nextCells;
-        this.paintToCanvas();
-    };
-
-    CellularAutomata.prototype.randomise = function() {
-        var i = 0;
-        for (var y = 0; y < height; y++) {
-            for (var x = 0; x < width; x++) {
-                cells[y][x] = Math.floor(20 * Math.random()) === 0;
-            }
-        }
-        this.paintToCanvas();
-    };
-
-    document.addEventListener('DOMContentLoaded', function() {
-        var canvas = document.querySelector('canvas');
-        canvas.width = innerWidth;
-        canvas.height = innerHeight;
-        global.ca = new CellularAutomata(canvas);
-    }, false);
-
-}(window));
+document.addEventListener('DOMContentLoaded', () => {
+  new CellularAutomata({
+    canvas: document.querySelector('canvas'),
+  });
+});
